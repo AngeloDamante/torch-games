c981ae8763ed40116ec289c0bf1a056b3c01d8c2
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-12-23 19:59:16 +0100

    wip: main update

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
+import sys
+from gui.torch_game import Ui_MainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class Comand_torch(QMainWindow):
+	def __init__(self):
+		super(Comand_torch, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
+
+		# [GUI] torch
+		self.ui.Torch_one.clicked.connect()
+		self.ui.Torch_two.clicked.connect()
+		self.ui.Torch_three.clicked.connect()
+		self.ui.Torch_four.clicked.connect()
+		self.ui.Torch_five.clicked.connect()
+
 
 def main():
-	pass
+	app = QApplication(sys.argv)
+	window = Comand_torch()
+	window.show()
+	sys.exit(app.exec())
+
 
 if __name__ == '__main__':
 	main()
