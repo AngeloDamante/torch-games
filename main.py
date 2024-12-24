544a6c5dbc974e9bafc26241c3275c456b38eb19
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-12-24 12:56:49 +0100

    wip: main update

--- main.py
+++ main.py
@@ -11,14 +11,25 @@ class Comand_torch(QMainWindow):
 		self.ui.setupUi(self)
 
 		# [GUI] torch
-		self.ui.Torch_one.clicked.connect(self.click_torch())
-		self.ui.Torch_two.clicked.connect(self.click_torch())
-		self.ui.Torch_three.clicked.connect(self.click_torch())
-		self.ui.Torch_four.clicked.connect(self.click_torch())
-		self.ui.Torch_five.clicked.connect(self.click_torch())
+		self.ui.Torch_one.clicked.connect(self.click_torch(1))
+		self.ui.Torch_two.clicked.connect(self.click_torch(2))
+		self.ui.Torch_three.clicked.connect(self.click_torch(3))
+		self.ui.Torch_four.clicked.connect(self.click_torch(4))
+		self.ui.Torch_five.clicked.connect(self.click_torch(5))
 
-	def click_torch(self):
-		pass
+	def click_torch(self, torch):
+		match torch:
+			case 1:
+				self.ui.Torch_one.setStyleSheet("background-color:red;")
+			case 2:
+				self.ui.Torch_two.setStyleSheet("background-color:red;")
+			case 3:
+				self.ui.Torch_three.setStyleSheet("background-color:red;")
+			case 4:
+				self.ui.Torch_four.setStyleSheet("background-color:red;")
+			case 5:
+				self.ui.Torch_five.setStyleSheet("background-color:red;")
+
 
 def main():
 	app = QApplication(sys.argv)
