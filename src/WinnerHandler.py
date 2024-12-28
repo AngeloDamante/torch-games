637d556251ea2e5ca78ec7b6f76614d061a854ee
Vittorione <vittorio.tiozzo.dev@gmail.com>
2024-12-28 17:22:49 +0100

    fix: renoved parenthesis

--- src/WinnerHandler.py
+++ src/WinnerHandler.py
@@ -5,8 +5,17 @@ Author: Vittorione
 Date: December 2024
 """
 
+import csv
+
+
 # TODO
 
-class WinnerHandler():
-    def __init__(self):
-        pass
+class WinnerHandler:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load_file(self):
+        with open(self.file_name, "r") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            results = [row for row in reader]
+        return results
