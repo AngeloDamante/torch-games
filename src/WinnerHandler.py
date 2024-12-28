ccd2eb709f459b0076c395267fba3750e2e2ca99
AngeloDamante <angelo.damante16@gmail.com>
2024-12-28 19:03:34 +0100

    feat: implemented WinnerHandler class

--- src/WinnerHandler.py
+++ src/WinnerHandler.py
@@ -2,20 +2,41 @@
 A simple class to handle winners. 
 
 Author: Vittorione
+CoAuthor: Angelone
 Date: December 2024
 """
 
+
+import os
 import csv
 
-
-# TODO
 
 class WinnerHandler:
     def __init__(self, file_name):
         self.file_name = file_name
+        self.winners = []
+        self.headers = ["Name", "Score"]
+        self.first_run()
+        self.load_winners()
 
-    def load_file(self):
-        with open(self.file_name, "r") as file:
+
+    def first_run(self):
+        # check that if csv file exists
+        if not os.path.exists(self.file_name):
+            with open(self.file_name, mode="w") as file:
+                writer = csv.DictWriter(file, fieldnames=self.headers, delimiter=";")
+                writer.writeheader()
+
+
+    def load_winners(self):
+        with open(self.file_name, mode="r") as file:
             reader = csv.DictReader(file, delimiter=";")
-            results = [row for row in reader]
-        return results
+            self.winners = [row for row in reader]
+
+
+    def get_winners(self):
+        return self.winners
+
+
+    def add_winner(self, name, score):  
+        pass
